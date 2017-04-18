13cc314ae06f30f34313e0ac0885617d49a3c65b
Marcin Szamotulski <mszamot@gmail.com>
2017-04-18 19:13:30 +0100

    refactor code

--- src/Redox/React.purs
+++ src/Redox/React.purs
@@ -7,14 +7,15 @@ module React.Redox
   ) where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Aff (Canceler)
-import Data.Maybe (Maybe(..))
-import Data.Lens (Lens', view)
-import ReactHocs (CONTEXT, withContext, accessContext, readContext, getDisplayName) 
 import React as R
-import React (ReactClass, ReactSpec, ReactThis)
 import Redox as Redox
+import Control.Monad.Aff (Canceler)
+import Control.Monad.Eff (Eff, kind Effect)
+import Control.Monad.Eff.Unsafe (unsafeCoerceEff)
+import Data.Lens (Lens', view)
+import Data.Maybe (Maybe(..))
+import React (ReactClass, ReactSpec, ReactThis)
+import ReactHocs (CONTEXT, withContext, accessContext, readContext, getDisplayName)
 import Redox (ReadRedox, SubscribeRedox, ReadWriteSubscribeRedox, Store, SubscriptionId)
 import Type.Proxy (Proxy(..))
 
@@ -30,9 +31,8 @@ withStore
   -> (Store state -> dsl -> Eff (ReadWriteSubscribeRedox eff) (Canceler (ReadWriteSubscribeRedox eff)))
   -> ReactClass props
   -> ReactClass props
-withStore store dispatch cls =
-  withContext cls { redox: {store, dispatch: dispatch store} }
-
+withStore store dispatch_ cls =
+  withContext cls { redox: { store, dispatch: dispatch_ store } }
 
 type RedoxContext state dsl eff =
   { redox ::
@@ -44,6 +44,54 @@ type RedoxContext state dsl eff =
 type RedoxStoreContext state = { redox :: { store :: Store state } }
 
 type ConnectState state = { state :: state, sid :: Maybe SubscriptionId }
+
+_connect
+  :: forall state state' props props' eff
+   . (ReactThis props' (ConnectState state')
+      -> Eff (context :: CONTEXT, readRedox :: ReadRedox, subscribeRedox :: SubscribeRedox | eff) (Store state))
+  -> Lens' state state'
+  -> (state' -> props' -> props)
+  -> ReactClass props
+  -> ReactSpec props' (ConnectState state') ( context :: CONTEXT, readRedox :: ReadRedox, subscribeRedox :: SubscribeRedox | eff )
+_connect storeEff _lns _iso cls = (R.spec' getInitialState renderFn)
+    { displayName = getDisplayName cls <> "Connect"
+    , componentWillMount = componentWillMount
+    , componentWillUnmount = componentWillUnmount
+    }
+  where
+
+    update this state = R.transformState this (_ { state = view _lns state })
+
+    getInitialState this = do
+      store <- addReactDisallowedEff $ storeEff this
+      state <- Redox.getState store
+      pure { state: view _lns state, sid: Nothing }
+
+    componentWillMount this = do
+      store <- addReactEff $ storeEff this
+      sid <- Redox.subscribe store $ update this
+      R.transformState this (_ { sid = Just sid })
+
+    componentWillUnmount this = do
+      store <- addReactReadOnlyEff $ storeEff this
+      msid <- (R.readState this) >>= pure <<< _.sid
+      case msid of
+        Nothing -> pure unit
+        Just sid -> Redox.unsubscribe store sid
+
+    renderFn this = do
+      props' <- R.getProps this
+      state <- R.readState this >>= pure <<< _.state
+      pure $ R.createElement cls (_iso state props') []
+
+    addReactEff :: forall a e. Eff e a -> Eff ( props :: R.ReactProps, state :: R.ReactState R.ReadWrite, refs :: R.ReactRefs (() :: # Effect) | e ) a
+    addReactEff = unsafeCoerceEff
+
+    addReactDisallowedEff :: forall a e. Eff e a -> Eff ( props :: R.ReactProps, state :: R.ReactState R.Disallowed, refs :: R.ReactRefs R.Disallowed | e ) a
+    addReactDisallowedEff = unsafeCoerceEff
+
+    addReactReadOnlyEff :: forall a e. Eff e a -> Eff ( props :: R.ReactProps, state :: R.ReactState R.ReadOnly, refs :: R.ReactRefs R.ReadOnly | e) a
+    addReactReadOnlyEff = unsafeCoerceEff
 
 -- | You must wrap the resulting component with `ReactHocs.accessContext` from
 -- | `purescript-react-hocs`.  Checkout `connect` bellow.  This function makes
@@ -62,36 +110,9 @@ connect'
   -> (state' -> props' -> props)
   -> ReactClass props
   -> ReactSpec props' (ConnectState state') ( context :: CONTEXT, readRedox :: ReadRedox, subscribeRedox :: SubscribeRedox | eff )
-connect' _lns _iso cls = (R.spec' getInitialState renderFn)
-    { displayName = getDisplayName cls <> "Connect"
-    , componentWillMount = componentWillMount
-    , componentWillUnmount = componentWillUnmount
-    }
+connect' _lns _iso cls = _connect storeEff _lns _iso cls
   where
-
-    update this state = R.transformState this (_ { state = view _lns state })
-
-    getInitialState this = do
-      store <- readContext (Proxy :: Proxy (RedoxStoreContext state)) this >>= pure <<< _.redox.store
-      state <- Redox.getState store
-      pure { state: view _lns state, sid: Nothing }
-
-    componentWillMount this = do
-      store <- readContext (Proxy :: Proxy (RedoxStoreContext state)) this >>= pure <<< _.redox.store
-      sid <- Redox.subscribe store $ update this
-      R.transformState this (_ { sid = Just sid })
-
-    componentWillUnmount this = do
-      store <- readContext (Proxy :: Proxy (RedoxStoreContext state)) this >>= pure <<< _.redox.store
-      msid <- R.readState this >>= pure <<< _.sid
-      case msid of
-        Nothing -> pure unit
-        Just sid -> Redox.unsubscribe store sid
-
-    renderFn this = do
-      props' <- R.getProps this
-      state <- R.readState this >>= pure <<< _.state
-      pure $ R.createElement cls (_iso state props') []
+    storeEff this = readContext (Proxy :: Proxy (RedoxStoreContext state)) this >>= pure <<< _.redox.store
 
 connect
   :: forall state state' props props'
