edd366d099a8381d74474261c1a930b6e72f2025
Marcin Szamotulski <mszamot@gmail.com>
2017-10-05 23:02:34 +0200

    Updated tests

--- test/Main.purs
+++ test/Main.purs
@@ -6,9 +6,9 @@ import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
 import Control.Monad.Eff.Exception (EXCEPTION)
-import Data.Function.Uncurried (runFn2, runFn3)
+import Data.Function.Uncurried (runFn3)
 import Data.Lens (lens, view)
-import React.Redox (unsafeShallowEqual, unsafeStrictEqual)
+import React.Redox (unsafeShallowEqual)
 import Redox (CreateRedox, RedoxStore)
 import Test.ServerSideRendering (testSuite) as SSR
 import Test.Unit (suite, test)
@@ -26,29 +26,6 @@ main :: forall eff. Eff
   ) Unit
 main = runTest do
   SSR.testSuite
-
-  suite "unsafeStrictEqual" do
-    test "record update"
-      let r = { field: "Hello" }
-      in do
-        assert "updated records should be unequal" $ not $ runFn2 unsafeStrictEqual r (r { field = "Hay" })
-
-    test "new records with the same content" do
-      -- not equal since we create a new top level Object
-      assert "new records with primitive value should not be equal" $ not $ runFn2 unsafeStrictEqual { field: 1 } { field: 1 }
-      let x = { x: 1 }
-      assert "new records should not be equal" $ not $ runFn2 unsafeStrictEqual { x } { x }
-
-    test "lens view"
-      let 
-        lens1_ = lens (_.field) (_ { field = _ })
-        lens2_ = lens (_.field) (_ { field = _ })
-        r1 = { field: { str: "Hello" } }
-        r2 = { field: ":)" }
-
-      in do
-        assert "lens view should be equal" $ runFn2 unsafeStrictEqual (view lens1_ r1) (view lens1_ r1)
-        assert "lens view should be equal" $ runFn2 unsafeStrictEqual (view lens2_ r2) (view lens2_ r2)
 
   suite "unsafeShallowEqual" do
     test "record update"
